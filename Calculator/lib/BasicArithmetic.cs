ce1c85419c957900d80a640dbe3ce759072f8587
Andre Luiz Abdalla Silveira <andreluizsa00@gmail.com>
2024-09-10 16:08:31 -0300

    Create abstract class to handle the arithmetic

---
+++ Calculator/lib/BasicArithmetic.cs
@@ -0,0 +1,38 @@
+using System.Numerics;
+
+namespace Calculator;
+
+public abstract class BasicArithmetic<T> where T : INumber<T>
+{
+    public T Add(params T[] a) => a.Aggregate((x, y) => x + y);
+    
+    public T Subtract(params T[] a) => a.Aggregate((x, y) => x - y);
+    
+    public T Multiply(params T[] a) => a.Aggregate((x, y) => x * y);
+
+    public T Divide(T a, T b)
+    {
+        if (b.Equals(default(T)))
+        {
+            throw new DivideByZeroException();
+        }
+        
+        return a / b;
+    }
+    
+    public (T result, T remainig) DivideRemainder(T a, T b)
+    {
+        if (b.Equals(default(T)))
+        {
+            throw new DivideByZeroException();
+        }
+
+        return (a / b, a % b);
+    }
+
+    public abstract T Power(T a, T b);
+
+    public abstract T Tetration(T a, T b);
+
+    public abstract T Factorial(T a);
+}
